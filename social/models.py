e61f718eef868c3b1d9eed3b576221d4137010cc
Gaidar <garius642@gmail.com>
2021-07-25 19:03:31 +0300

    Post model created

--- social/models.py
+++ social/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    content = models.TextField(max_length=1024)
+
+    def __repr__(self) -> str:
+        return f"<{self.title}>"
